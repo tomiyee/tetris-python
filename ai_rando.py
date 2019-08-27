4fc0613d476e524db05fa3023978a3b0f3876f04
tomiyee <tommy.and.heng@gmail.com>
2019-08-26 20:05:36 -0400

    Bare Minimum

---
+++ ai_rando.py
@@ -0,0 +1,22 @@
+
+from random import randrange as rand
+
+class Model:
+    def __init__ (self):
+        pass
+
+    def next_move (self):
+        """
+        Returns a String, the keyboard name
+
+        Either UP, DOWN, LEFT, or RIGHT
+        """
+        x = rand(4)
+        if x == 0:
+            return 'UP'
+        elif x == 1:
+            return 'DOWN'
+        elif x == 2:
+            return 'LEFT'
+        elif x == 3:
+            return 'RIGHT'
